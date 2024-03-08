c6a2b55aa537938097fea8a111a79bc05b96c5eb
estudanteandreceep <124880847+andreferreiradeandrade@users.noreply.github.com>
2024-03-08 09:24:16 -0300

    Add files via upload

---
+++ php/sin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>arquivo php</title>
+    <link rel="stylesheet" href="../css/estilo.css">
+    </head>
+<body>
+
+
+<?php
+
+$val = $_GET["val"];;
+echo(sin($val));
+ 
+?>
+
+
+<br>
+<a href="http://localhost/Andre/funcoes/index.html">voltar</a>
+
+</body>
+</html>
